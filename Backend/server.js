0f041cc8352fadba6093afd7329c09ec6638dd1a
Daniel Obasi <danielobasy22@gmail.com>
2026-02-04 13:29:34 +0100

    Refactor server.js for improved readability

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require("./config/db");
 const bcrypt = require("bcryptjs");
-const User = require("./models/User");
+const User = require("./models/user");
 
 dotenv.config();
 const app = express();
@@ -57,4 +57,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(` Server running on port ${PORT}`);
+
 });
