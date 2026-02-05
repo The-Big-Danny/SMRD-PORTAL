9abf01b66dbd6ed3449236aa0ec11b41489d2aa0
Daniel Obasi <danielobasy22@gmail.com>
2026-02-05 12:56:20 +0100

    Refactor studentController for consistency and clarity

--- Backend/controllers/studentController.js
+++ Backend/controllers/studentController.js
@@ -1,4 +1,4 @@
-const Student = require("../models/Student");
+const Student = require("../models/student");
 const Result = require("../models/result");
 const bcrypt = require("bcryptjs"); // Don't forget to import this!
 
@@ -82,4 +82,5 @@ exports.getStudentDashboard = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "Error loading dashboard", error: error.message });
   }
+
 };
