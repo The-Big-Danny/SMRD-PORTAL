a43a1c890c86396fff592210b92cde14e951316e
Daniel Obasi <danielobasy22@gmail.com>
2026-02-12 10:39:15 +0100

    Delete Backend/routes/studentRoutes.js

--- Backend/routes/studentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { registerStudent } = require("../controllers/studentController");
-
-router.post("/register", registerStudent);
-
-module.exports = router;
